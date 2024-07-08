956c43074279fed9ce7a838ee12af7c6bec9cdcd
Ayan <karmakarayan217@gmail.com>
2024-07-08 15:08:51 +0530

    working on login

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('api')->group(function () {
     Route::get('users', [App::class, 'getUsers']);
     Route::post('register', [App::class, 'register']);
+    Route::post('login', [App::class, 'login']);
 });
 
 Route::get('{view}', [App::class, 'index'])->where('view', '(.*)');
